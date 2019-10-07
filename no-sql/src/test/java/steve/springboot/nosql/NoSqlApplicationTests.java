dbed6b35101a4566f8f2004416c6f988924e935f
stevevenzearo <2226069315@qq.com>
2019-10-07 23:55:26 +0800

    noSQL init

---
+++ no-sql/src/test/java/steve/springboot/nosql/NoSqlApplicationTests.java
@@ -0,0 +1,16 @@
+package steve.springboot.nosql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoSqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
