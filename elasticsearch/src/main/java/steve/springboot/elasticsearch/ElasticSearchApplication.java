50f7b533fbe66282f4e61ebca359f24d2afc1129
steve <stevenzearo@gmail.com>
2019-10-23 18:13:23 +0800

    elastic search

---
+++ elasticsearch/src/main/java/steve/springboot/elasticsearch/ElasticSearchApplication.java
@@ -0,0 +1,14 @@
+package steve.springboot.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author steve
+ */
+@SpringBootApplication
+public class ElasticSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchApplication.class, args);
+    }
+}
