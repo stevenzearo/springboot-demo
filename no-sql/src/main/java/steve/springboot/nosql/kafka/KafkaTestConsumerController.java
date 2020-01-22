ed9b1d2c6fd6b38f13affbbf3ea6ddec1e8d26e5
steve <stevenzearo@gmail.com>
2020-01-22 15:17:22 +0800

    kafka

--- no-sql/src/main/java/steve/springboot/nosql/kafka/KafkaTestConsumerController.java
+++ no-sql/src/main/java/steve/springboot/nosql/kafka/KafkaTestConsumerController.java
@@ -1,7 +1,9 @@
 package steve.springboot.nosql.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.KafkaListeners;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test/kafka")
 @RestController
 public class KafkaTestConsumerController {
+    @Autowired
+    ConsumerRecord<String, String> record;
 
-    @KafkaListener(topics = {"test"})
-    @RequestMapping(value = "/message", method= RequestMethod.GET)
+    @KafkaListener(topics = {"test"}, groupId = "test")
+    @RequestMapping(value = "/message", method = RequestMethod.GET)
     public String getMessage(ConsumerRecord<String, String> record) throws Exception {
         return String.format("topic = %s, offset = %d, value = %s \n", record.topic(), record.offset(), record.value());
     }
--- no-sql/src/main/java/steve/springboot/nosql/kafka/KafkaTestProducerController.java
+++ no-sql/src/main/java/steve/springboot/nosql/kafka/KafkaTestProducerController.java
@@ -2,6 +2,7 @@ package steve.springboot.nosql.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequestMapping("/test/kafka")
 @RestController
+@Component
 public class KafkaTestProducerController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @RequestMapping(value = "/message", method= RequestMethod.POST)
+    @RequestMapping(value = "/message", method = RequestMethod.POST)
     public String sendMessage(@RequestBody String message) {
         kafkaTemplate.send("test", message);
         return "message send successfully";
