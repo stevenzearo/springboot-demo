db5bdf8048dab3eb6b6e4918b2a4256bb07b24be
steve <2226069315@qq.com>
2024-02-26 14:23:04 +0800

    Add JPA demo

--- jpa-demo/src/main/java/com/demo/jpa/dao/IdentityRepository.java
+++ jpa-demo/src/main/java/com/demo/jpa/dao/IdentityRepository.java
@@ -1,7 +1,9 @@
 package com.demo.jpa.dao;
 
+
 import com.demo.jpa.domain.Identity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Author  ZLH
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Time  15:42
  * Version  1.0
  */
+@Repository
 public interface IdentityRepository extends JpaRepository<Identity, Integer> {
-    Identity getUserById(Integer id);
+    Identity getById(String id);
 }
