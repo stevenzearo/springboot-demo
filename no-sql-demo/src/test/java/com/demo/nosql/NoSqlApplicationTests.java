612ea9bca81cb997efc71818040bfbbe1b8b61b9
邹亮华 <zou.lianghua@infypower.cn>
2024-02-27 17:47:56 +0800

    1. refactor 2. add mongodb demo

--- no-sql-demo/src/test/java/com/demo/nosql/NoSqlApplicationTests.java
+++ no-sql-demo/src/test/java/com/demo/nosql/NoSqlApplicationTests.java
@@ -1,8 +1,10 @@
 package com.demo.nosql;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 public class NoSqlApplicationTests {
 
