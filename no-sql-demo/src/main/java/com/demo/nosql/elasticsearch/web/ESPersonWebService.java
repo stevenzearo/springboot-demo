beb66ffe5012654250e439017595572fe69e6394
steve <2226069315@qq.com>
2024-02-29 19:21:54 +0800

    refactoring of elasticsearch

--- no-sql-demo/src/main/java/com/demo/nosql/elasticsearch/web/ESPersonWebService.java
+++ no-sql-demo/src/main/java/com/demo/nosql/elasticsearch/web/ESPersonWebService.java
@@ -1,25 +1,24 @@
 package com.demo.nosql.elasticsearch.web;
 
 import com.demo.nosql.elasticsearch.domain.Person;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * @author steve
  */
-@RequestMapping("/ajax")
+@RestController
 public interface ESPersonWebService {
-    @RequestMapping(value = "/api/person", method = RequestMethod.POST)
-    Person save(Person person);
+    @RequestMapping(value = "/api/es/person", method = RequestMethod.POST)
+    Person save(@RequestBody Person person);
 
-    @RequestMapping(value = "/api/person", method = RequestMethod.GET)
-    Person get(String id);
+    @RequestMapping(value = "/api/es/person/{id}", method = RequestMethod.GET)
+    Person get(@PathVariable String id);
 
-    @RequestMapping(value = "/api/person", method = RequestMethod.PUT)
-    List<Person> search(Person person);
+    @RequestMapping(value = "/api/es/person", method = RequestMethod.PUT)
+    List<Person> search(@RequestBody Person person);
 
-    @RequestMapping(value = "/api/person", method = RequestMethod.DELETE)
-    void delete(String id);
+    @RequestMapping(value = "/api/es/person/{id}", method = RequestMethod.DELETE)
+    void delete(@PathVariable String id);
 }
--- no-sql-demo/src/main/java/com/demo/nosql/elasticsearch/web/impl/ESPersonServiceImpl.java
+++ no-sql-demo/src/main/java/com/demo/nosql/elasticsearch/web/impl/ESPersonServiceImpl.java
@@ -2,10 +2,9 @@ package com.demo.nosql.elasticsearch.web.impl;
 
 import com.demo.nosql.elasticsearch.web.ESPersonWebService;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import com.demo.nosql.elasticsearch.domain.Person;
 import com.demo.nosql.elasticsearch.service.ESPersonService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,28 +12,28 @@ import java.util.List;
 /**
  * @author steve
  */
-@RestController
+@Component
 public class ESPersonServiceImpl implements ESPersonWebService {
     @Resource
-    ESPersonService personService;
+    ESPersonService esPersonService;
 
     @Override
     public Person save(Person person) {
-        return personService.create(person);
+        return esPersonService.create(person);
     }
 
     @Override
     public Person get(String id) {
-        return personService.get(id);
+        return esPersonService.get(id);
     }
 
     @Override
     public List<Person> search(Person person) {
-        return personService.search(person);
+        return esPersonService.search(person);
     }
 
     @Override
     public void delete(String id) {
-        personService.delete(id);
+        esPersonService.delete(id);
     }
 }
