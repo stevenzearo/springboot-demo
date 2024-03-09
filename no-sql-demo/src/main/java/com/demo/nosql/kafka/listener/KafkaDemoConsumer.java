18f607461abcb1d14b696a17570ee02a52762fc9
steve <2226069315@qq.com>
2024-03-09 20:06:50 +0800

    Fix kafka deploying

--- no-sql-demo/src/main/java/com/demo/nosql/kafka/listener/KafkaDemoConsumer.java
+++ no-sql-demo/src/main/java/com/demo/nosql/kafka/listener/KafkaDemoConsumer.java
@@ -2,7 +2,6 @@ package com.demo.nosql.kafka.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
@@ -11,12 +10,12 @@ import org.springframework.stereotype.Component;
 /**
  * @author steve
  */
-//@Component
-//@EnableKafka
+@Component
+@EnableKafka
 public class KafkaDemoConsumer {
     private static final Logger logger = LoggerFactory.getLogger(KafkaDemoConsumer.class);
 
-//    @KafkaListener(topics = {"test"} )
+    @KafkaListener(topics = {"test"} )
     public void listen(String value, ConsumerRecordMetadata recordMetadata) throws Exception {
         String info = String.format("topic = %s,  offset = %d, value = %s \n", recordMetadata.topic(), recordMetadata.offset(), value);
         logger.info(info);
